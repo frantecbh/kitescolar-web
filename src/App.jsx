75e8e56622f99100d42f5a364e943cb56bc59425
frantecbh <franciscomenezesbh@gmail.com>
2023-07-24 15:09:43 -0300

    rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { AuthProvider } from './Context/AuthContext'
+import { Dashboard } from './pages/Dashboard'
 import { Rotas } from './routes'
 
 export default function App() {
-  return <Rotas />
+  return <Dashboard />
 }
